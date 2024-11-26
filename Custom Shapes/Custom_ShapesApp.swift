cfa4293d110a193fa435adbec9a3c28fc43f08b6
Joseph Ji <jji@lcs.on.ca>
2024-11-26 14:22:16 -0500

    Initial Commit

---
+++ Custom Shapes/Custom_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ShapesApp.swift
+//  Custom Shapes
+//
+//  Created by Xinchen Ji on 2024-11-26.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
